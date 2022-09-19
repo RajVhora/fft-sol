ad1d9e8cbb9941d4babc1c780f95d8f54315e612
RajVhora <rajvhora2000@outlook.com>
2022-09-19 15:38:15 +0530

    Changed order of plotting in plot.py

--- plot.py
+++ plot.py
@@ -8,8 +8,8 @@ print("Graphing has begun")
 data = np.loadtxt('numerical.dat')
 
 # Plot data
-plt.plot(data[:,0], data[:,2], label='sigma_22')
 plt.plot(data[:,0], data[:,1], label='sigma_11')
+plt.plot(data[:,0], data[:,2], label='sigma_22')
 plt.plot(data[:,0], data[:,3], label='sigma_12')
 plt.xlabel('x')
 plt.ylabel('sigma')
